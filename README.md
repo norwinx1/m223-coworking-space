6395936c17e67bd3f03f8ae4eb57947f1fb0d496
Norwin Schaefer <norwin.schaefer@ergon.ch>
2022-12-07 13:29:25 +0000

    feat: implemented password hash

--- README.md
+++ README.md
@@ -10,3 +10,6 @@ Es wird ein Admin, zwei Mitglieder und ein paar Buchungen erstellt, welche diese
 ## Änderungen gegenüber der Planung
 * Der Endpunkt /bookings/cancel/{id} kann den Status FORBIDDEN werfen, wenn dem User die ausgewählte Buchung nicht gehört.
 * Im Enum *Role* gibt es den Wert VISITOR nicht mehr.
+
+## Hinweise
+* Das Salt für den Passwort-Hash ist aus Einfachheitsgründen für jeden User gleich und hardcoded.
--- src/main/java/ch/zli/m223/service/PasswordService.java
+++ src/main/java/ch/zli/m223/service/PasswordService.java
@@ -1,6 +1,14 @@
 package ch.zli.m223.service;
 
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
 import javax.enterprise.context.ApplicationScoped;
+
+import org.apache.commons.codec.binary.Hex;
 
 /*
  * Sligthly changed from source: https://medium.com/@kasunpdh/how-to-store-passwords-securely-with-pbkdf2-204487f14e84
@@ -8,7 +16,26 @@ import javax.enterprise.context.ApplicationScoped;
 @ApplicationScoped
 public class PasswordService {
     public String hashPassword(String password) {
-        //TODO implement hashing
-        return password;
+        String salt = "0923lkynsdgl0293m,.äpo7blksd";
+        int iterations = 10000;
+        int keyLength = 512;
+        char[] passwordChars = password.toCharArray();
+        byte[] saltBytes = salt.getBytes();
+
+        byte[] hashedBytes = hash(passwordChars, saltBytes, iterations, keyLength);
+        return Hex.encodeHexString(hashedBytes);
+    }
+
+    public static byte[] hash(final char[] password, final byte[] salt, final int iterations,
+            final int keyLength) {
+        try {
+            SecretKeyFactory skf = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA512");
+            PBEKeySpec spec = new PBEKeySpec(password, salt, iterations, keyLength);
+            SecretKey key = skf.generateSecret(spec);
+            byte[] res = key.getEncoded();
+            return res;
+        } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
